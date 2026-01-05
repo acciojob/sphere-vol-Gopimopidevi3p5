2c4cac0c8ff698d060e0448f3e8e1d6736ce9f2b
Gopimopidevi3p5 <ravaagopi3p5@gmail.com>
2026-01-05 13:01:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
+	e.preventDefault()
+	const radius=Number(e.target.radius.value)
+	const valume=e.target.volume
+	if(isNaN(radius) || radius<=0){
+		valume.value="NaN"
+		return
+}
+	valume.value=(4/3 * Math.PI  * Math.pow(radius,3)).toFixed(4)
+	
   
 } 
 
