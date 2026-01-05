58a3a6fc2316b374feb99ecf512cbd4b948a62f6
Gopimopidevi3p5 <ravaagopi3p5@gmail.com>
2026-01-05 13:09:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function volume_sphere(e) {
     //Write your code here
 	e.preventDefault()
 	const radius=Number(e.target.radius.value)
-	const valume=e.target.volume
+	const valumeEle=e.target.volume
 	if(isNaN(radius) || radius<=0){
 		valume.value="NaN"
 		return
 }
-	valume.value=(4/3 * Math.PI  * Math.pow(radius,3)).toFixed(4)
+	const valume=(4/3 * Math.PI  * Math.pow(radius,3))
+	const roundedVolume = Math.round(valume * 10000)/10000
+	valumeEle.value=roundedVolume
 	
   
 } 
